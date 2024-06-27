76ce9a08b8ee5673d25aa4e5dd6d92d4c4827e52
mambafcuk <kartik.karanwal.powerbi@gmail.com>
2024-06-27 15:14:24 +0000

    variables

---
+++ .devcontainer/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12232
+let accountEmail = "kartikkaranwal8192@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+
+// accountId = 12345 Not allowed
+
+console.log(accountId)
+
+/*
+Prefer not use the var
+because of block scope  & functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
