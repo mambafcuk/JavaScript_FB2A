7c55b7aee4547ec0ac4e4c04e62b34200b5941f4
mambafcuk <kartik.karanwal.powerbi@gmail.com>
2024-06-28 08:46:40 +0000

    Strings

---
+++ Basics_01/strings.js
@@ -0,0 +1,7 @@
+const name =  "Kartik"
+const skill = " Data Analyst"
+console.log(`My name is ${name} & I'm a ${skill}`) //Don''t use contactnation for the string
+
+const newName = new String('   Kartik  ')
+console.log(newName)
+console.log(newName.trim())
