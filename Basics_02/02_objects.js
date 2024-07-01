0ab77968cf6cb992733f4e3609220e5d185e1895
mambafcuk <kartik.karanwal.powerbi@gmail.com>
2024-07-01 05:21:41 +0000

    Objects

--- Basics_02/02_objects.js
+++ Basics_02/02_objects.js
@@ -34,3 +34,45 @@ JSUser.greeting = function(){
 // Singleton Object
 
 
+const regularName = {
+       email : "kartikkaranwal8192@gmail.com",
+       fullName:{
+        userName:{
+            firstName : "kartik",
+            lastName : "karanwal"
+        }
+       }
+}
+
+// console.log(regularName.fullName.userName.firstName);
+
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "b", 4: "d"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+
+
+// console.log(Object.keys(obj1))
+// console.log(Object.values(obj1));
+// console.log(Object.entries(obj1))
+
+// console.log(Object.hasOwnProperty('a'))
+
+
+const course ={
+    courseId : 1001,
+    courseName : "Javascript",
+    coursePrice : 599
+}
+
+
+const {courseId, courseName, coursePrice} = course
+
+
+console.log(coursePrice);
