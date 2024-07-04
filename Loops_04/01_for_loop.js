76ca5457092db5d312461454e69dfd437f2a8999
mambafcuk <kartik.karanwal.powerbi@gmail.com>
2024-07-04 10:25:21 +0000

    Loops

---
+++ Loops_04/01_for_loop.js
@@ -0,0 +1,16 @@
+for (let i = 0; i <= 20; i++) {
+   
+    console.log(`${i}`)
+    
+}
+
+
+for (let i = 0; i <= 20; i++) {
+ 
+    console.log(` Outer Loop is ${i}`);
+    for (let j = 0; j <= 20; j++) {
+       console.log(`Inner Loop is ${j} & inner Loop is ${i}`);
+        
+    }
+    
+}
---
+++ Loops_04/02_do_while_loop.js
@@ -0,0 +1,17 @@
+// While Loop
+
+i=0;
+while (i<=10) {
+//   console.log(`${i}`)
+  i++;  
+}
+
+
+// Do-While Loop
+let score=0
+
+
+do {
+    console.log(`score value is ${score}`)
+    score++;
+} while (score <= 10);
